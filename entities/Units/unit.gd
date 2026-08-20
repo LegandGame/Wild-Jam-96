c21834e66dc861cd4b0854603e80f3def7081627
Legand <70213999+LegandGame@users.noreply.github.com>
2026-08-19 20:56:00 -0400

    enemy pathfinding

--- entities/Units/unit.gd
+++ entities/Units/unit.gd
@@ -13,12 +13,15 @@ static var unit_list := {
 	"ENEMY" : []
 }
 
-signal unit_list_updated(unit_alignment : String)
-
 func _ready() -> void:
 	_bug_checker()
 	_init_stats()
 	_connect_signals()
+	_update_targets()
+	if stats.alignment == "ALLY":
+		_find_new_target("ENEMY")
+	elif stats.alignment == "ENEMY":
+		_find_new_target("ALLY")
 
 func _bug_checker() -> void:
 	assert(stats)
@@ -37,24 +40,37 @@ func _connect_signals() -> void:
 	hurtbox.hurt.connect(_on_hurtbox_hurt)
 	health.value_empty.connect(_die)
 
+func _update_targets() -> void:
+	match stats.alignment:
+		"ALLY":
+			for u in unit_list["ENEMY"]:
+				u._find_new_target("ALLY")
+		"ENEMY":
+			for u in unit_list["ALLY"]:
+				u._find_new_target("ENEMY")
+
 # -------------
 func _on_hurtbox_hurt(hitbox : Hitbox) -> void:
 	health.value -= hitbox.damage
 
 func _die() -> void:
 	unit_list[stats.alignment].erase(self)
+	_update_targets()
 	queue_free()
 
 # Nav mesh Stuff
 func _find_new_target(unit_alignment : String) -> void:
 	## unit that spawned/died shares our alignemnt, in which no need to do anything
+	print("Alignment Check: ", stats.alignment, " ", unit_alignment, " ", self)
 	if unit_alignment == stats.alignment:
 		return
 	var temp_sorted = unit_list[unit_alignment]	# we only need to create a temprary shallow array
+	if len(temp_sorted) < 1:
+		return
 	temp_sorted.sort_custom(pos_sort)
 	var closest_target = temp_sorted[0]
 	
-	
+	navigator.target_position = closest_target.position
 
 func update_target_location(target_pos : Vector3) -> void:
 	navigator.target_position = target_pos
@@ -65,9 +81,12 @@ func _physics_process(delta: float) -> void:
 	var move_dir := (next_location - cur_location).normalized()
 	velocity = velocity.move_toward(move_dir * stats.move_speed, stats.ACCEL * delta)
 	
+	if not is_on_floor():
+		velocity.y += get_gravity().y
+	
 	move_and_slide()
 
-# --- misc.
+# --- misc and utils
 func pos_sort(a : Node3D, b : Node3D) -> bool:
 	var da := a.global_position.distance_squared_to(self.global_position)
 	var db := b.global_position.distance_squared_to(self.global_position)
