1e2c75b0f4afc451eba1d0f058bfc786ff2a5d42
Nic <130799100+SpazticSqurrel@users.noreply.github.com>
2026-08-22 14:33:12 -0500

    implement environment, units
    navigation strains my last dusty brain cell

--- entities/Units/unit.gd
+++ entities/Units/unit.gd
@@ -1,12 +1,13 @@
 class_name Unit extends CharacterBody3D
 
+@export_enum("ALLY", "ENEMY") var alignment := "ALLY"
+
 @export_category("Node References")
 @export var stats : UnitStats
 @export var model : AnimatedCharacter
 @export var health : Counter
-@export var hurtbox : Hurtbox
 @export var navigator : NavigationAgent3D
-@export var hitbox : Hitbox
+@export var line_of_sight : RayCast3D
 
 ## dictionary that keeps track of all currently active units based on their alignment
 static var unit_list := {
@@ -15,6 +16,11 @@ static var unit_list := {
 }
 
 var attacking := false
+var time_alive := 0.0
+var time_since_path_update := 0.0
+var look_direction := Vector3.FORWARD
+var path_location := self.position
+var target_reached := false
 var target : Unit	# enemy unit we are currently moving towards
 
 func _ready() -> void:
@@ -22,35 +28,30 @@ func _ready() -> void:
 	_init_stats()
 	_connect_signals()
 	_update_targets()
-	if stats.alignment == "ALLY":
+	if alignment == "ALLY":
 		_find_new_target("ENEMY")
-	elif stats.alignment == "ENEMY":
+	elif alignment == "ENEMY":
 		_find_new_target("ALLY")
 
 func _bug_checker() -> void:
 	assert(stats)
 	assert(model)
 	assert(health)
-	assert(hurtbox)
 	assert(navigator)
-	assert(hitbox)
-	hitbox.monitorable = false
-	hitbox.monitoring = false
 
 func _init_stats() -> void:
 	health.max_value = stats.health
 	health.reset()
 	model.move_speed = stats.move_speed
-	self.add_to_group(stats.alignment)
-	unit_list[stats.alignment].append(self)
-	hitbox.damage = stats.damage
+	self.add_to_group(alignment)
+	unit_list[alignment].append(self)
 
 func _connect_signals() -> void:
-	hurtbox.hurt.connect(_on_hurtbox_hurt)
+	#hurtbox.hurt.connect(_on_hurtbox_hurt)
 	health.value_empty.connect(_die)
 
 func _update_targets() -> void:
-	match stats.alignment:
+	match alignment:
 		"ALLY":
 			for u in unit_list["ENEMY"]:
 				u._find_new_target("ALLY")
@@ -59,20 +60,22 @@ func _update_targets() -> void:
 				u._find_new_target("ENEMY")
 
 # -------------
-func _on_hurtbox_hurt(hitbox : Hitbox) -> void:
-	health.value -= hitbox.damage
+func hurt(attacker: Unit) -> void:
+	if !is_instance_valid(attacker): return
+	velocity *= Vector3(0.0, 1.0, 0.0)
+	health.value -= attacker.stats.damage
 	model.play_one_shot("HitReaction")
 
 func _die() -> void:
 	model.state = "Dead"
-	unit_list[stats.alignment].erase(self)
+	unit_list[alignment].erase(self)
 	_update_targets()
 	queue_free()
 
 # Nav mesh Stuff
 func _find_new_target(unit_alignment : String) -> void:
 	## unit that spawned/died shares our alignemnt, in which no need to do anything
-	if unit_alignment == stats.alignment:
+	if unit_alignment == alignment:
 		return
 	var temp_sorted = unit_list[unit_alignment]	# we only need to create a temprary shallow array
 	if len(temp_sorted) < 1:
@@ -86,42 +89,71 @@ func update_target_location(target_pos : Vector3) -> void:
 	navigator.target_position = target_pos
 
 func _physics_process(delta: float) -> void:
-	if is_instance_valid(target) and !attacking:
-		navigator.target_position = target.position
-	else:
-		navigator.target_position = self.position
+	time_alive += delta
+	time_since_path_update += delta
 	
-	if _target_in_range() or navigator.is_target_reached():
-		velocity = velocity.move_toward(Vector3.ZERO, stats.ACCEL * delta)
-		if is_instance_valid(target):
+	if not is_on_floor():
+		velocity.y += get_gravity().y
+	
+	# to let the spawn animation play out
+	if time_alive <= 1.0:
+		return
+	
+	if time_since_path_update >= 0.5:
+		update_path()
+	
+	path_location = get_path_location()
+	
+	if _target_in_range() or target_reached:
+		velocity = velocity.move_toward(Vector3.ZERO, stats.acceleration * delta)
+		if is_instance_valid(target) and !attacking:
+			look_direction = (target.global_position - self.global_position).normalized()
 			attack()
 	else:
-		var cur_location := global_transform.origin
-		var next_location := navigator.get_next_path_position()
-		var move_dir := (next_location - cur_location).normalized()
-		velocity = velocity.move_toward(move_dir * stats.move_speed, stats.ACCEL * delta)
-		rotation.y = lerp_angle(rotation.y, atan2(move_dir.x, move_dir.z), stats.ACCEL * delta)
+		var move_dir := (path_location - self.global_position).normalized()
+		look_direction = move_dir
+		velocity = velocity.move_toward(move_dir * stats.move_speed, stats.acceleration * delta)
+	
+	global_rotation.y = lerp_angle(global_rotation.y, atan2(look_direction.x, look_direction.z), stats.acceleration * delta)
 	
 	if !flatten_vector(velocity).is_zero_approx():
 		model.state = "Walking"
 	else:
 		model.state = "Idle"
 	
-	if not is_on_floor():
-		velocity.y += get_gravity().y
-	
 	move_and_slide()
+
+func update_path():
+	time_since_path_update = 0.0
+	if can_see_enemy(true): return
+	
+	if is_instance_valid(target) and navigator.target_position.distance_squared_to(target.global_position) >= 36.0:
+		navigator.target_position = target.global_position
+
+func get_path_location():
+	if can_see_enemy(true):
+		target_reached = _target_in_range()
+		return target.position
+	target_reached = navigator.is_target_reached()
+	return navigator.get_next_path_position()
+
+func can_see_enemy(override_target := false):
+	if !line_of_sight or !is_instance_valid(target): return false
+	line_of_sight.look_at(target.position, Vector3.UP, true)
+	var colliding_with: Node3D = line_of_sight.get_collider()
+	if is_instance_valid(colliding_with) and colliding_with is Unit and colliding_with.alignment != self.alignment:
+		if override_target:
+			target = colliding_with
+		return colliding_with
+	return false
 
 # --- misc and utils
 func attack() -> void:
 	if attacking: return
 	attacking = true
-	hitbox.set_deferred("monitorable", true)
-	hitbox.set_deferred("monitoring", true)
 	model.play_one_shot("Attack")
+	target.hurt(self)
 	await model.attack_finished
-	hitbox.set_deferred("monitorable", false)
-	hitbox.set_deferred("monitoring", false)
 	attacking = false
 
 func pos_sort(a : Node3D, b : Node3D) -> bool:
@@ -133,4 +165,4 @@ func flatten_vector(x: Vector3) -> Vector3:
 	return x * Vector3(1.0, 0.0, 1.0)
 
 func _target_in_range() -> bool:
-	return global_position.distance_to(navigator.target_position) < stats.attack_range
+	return self.global_position.distance_to(target.position) <= stats.attack_range if is_instance_valid(target) else false
