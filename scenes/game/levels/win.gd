bbfb8f8759008c9baa585a76f88c753f7677185b
Reno Thiart <rhthiart@gmail.com>
2026-08-23 03:10:10 -0700

    add back to main menu button on win screen

--- scenes/game/levels/win.gd
+++ scenes/game/levels/win.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export_file_path() var main_menu_path = "res://ui/opening/opening.tscn"
+
 @export var win_text = '''
 Finally...                           
     The [color=deepskyblue][b]OmniSpark[/b][/color] is charged...       
@@ -7,14 +9,29 @@ Finally...
 [font_size=50]And the [font_size=60][color=gray][b]Castle[/b][/color][/font_size] is safe![/font_size]
 '''
 
+@export var you_win = '''  [wave]You Win![/wave]
+[shake][font_size=50]The Knights are ec[color=skyblue]static[/color]![/font_size][/shake]
+'''
+
 func _ready():
+	# initialize parts of scene
 	$TextLabel.visible_ratio = 0.0
 	$TextLabel.text = win_text
 	
 	$YouWin.modulate.a = 0
+	$YouWin.text = you_win
 	
+	$Button.visible = false
+	
+	# fade in from black
+	var fade_in_tween = create_tween()
+	fade_in_tween.tween_property($ColorRect, "color", Color(0,0,0,0), 3)
+	await fade_in_tween.finished
+	
+	# wait a bit
 	await get_tree().create_timer(2).timeout
 	
+	# scroll story text
 	var text_tween = create_tween()
 	text_tween.tween_property($TextLabel, "visible_ratio", 1.0, 10)
 	
@@ -23,10 +40,18 @@ func _ready():
 	await text_tween.finished
 	await get_tree().create_timer(2).timeout
 	
+	# fade story text out, start win music
 	var fade_tween = create_tween()
 	fade_tween.tween_property($TextLabel, "modulate:a", 0, 3)
 	AudioManager.play_win_music()
 	await fade_tween.finished
 	
+	# fade in win text
 	var win_tween = create_tween()
 	win_tween.tween_property($YouWin, "modulate:a", 1, 3)
+	
+	await get_tree().create_timer(3).timeout
+	$Button.visible = true
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file(main_menu_path)
