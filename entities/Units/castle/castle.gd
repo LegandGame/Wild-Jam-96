3c48656c2cb50f7167f921df799fae09478bad25
Legand <70213999+LegandGame@users.noreply.github.com>
2026-08-23 13:41:39 -0400

    added charge damage

---
+++ entities/Units/castle/castle.gd
@@ -0,0 +1,40 @@
+extends Unit
+
+var charge_manager : ChargeManager
+var drain_rate := 1.0
+var units_overlayed : int = 0
+@onready var area_3d: Area3D = $Area3D
+
+
+func _ready() -> void:
+	alignment = "ALLY"
+	unit_list["ALLY"].append(self)
+	charge_manager = get_tree().get_first_node_in_group("charge_manager")
+	self.add_to_group("ALLY")
+	area_3d.body_entered.connect(_on_body_entered)
+	area_3d.body_exited.connect(_on_body_exited)
+
+func hurt(attacker: Unit) -> void:
+	if not is_instance_valid(attacker):
+		return
+	charge_manager._on_shield_attack(attacker.stats.damage)
+
+func _on_body_entered(body : Node3D) -> void:
+	if body is not Unit:
+		return
+	body = body as Unit
+	if body.alignment != "ENEMY":
+		return
+	units_overlayed += 1
+func _on_body_exited(body : Node3D) -> void:
+	if body is not Unit:
+		return
+	body = body as Unit
+	if body.alignment != "ENEMY":
+		return
+	units_overlayed -= 1
+
+func _physics_process(delta: float) -> void:
+	if units_overlayed >= 1:
+		print("CORRECT")
+		charge_manager._on_shield_attack(units_overlayed * drain_rate * delta)
