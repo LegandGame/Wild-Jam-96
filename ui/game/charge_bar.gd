09ec8d31135b9c35a5a5b882ed4f3aa8df4ac3c8
Reno Thiart <rhthiart@gmail.com>
2026-08-22 18:31:33 -0700

    upgraded charge bar

--- ui/game/charge_bar.gd
+++ ui/game/charge_bar.gd
@@ -1,4 +1,5 @@
-class_name ChargeBar extends ProgressBar
+class_name ChargeBar extends TextureProgressBar
 
 func _on_charge_modified(p_value):
 	value = p_value
+	$Label.text = "%.0f" % ((value / max_value) * 100) + "%"
